708d8dce7598fdeeab333cb329293ef38d146f31
renosyah <syahputrareno975@gmail.com>
2022-09-07 17:01:41 +0700

    remove beeping hp bar

--- assets/ui/bar/hp_bar.gd
+++ assets/ui/bar/hp_bar.gd
@@ -4,7 +4,6 @@ onready var _hp_bar = $hp_bar
 onready var _hp_bar_bg = $hp_bar_bg
 onready var _tween = $Tween
 onready var _label = $RichTextLabel
-onready var _animation = $AnimationPlayer
 
 func show_label(_show = true):
 	_label.visible = _show
@@ -22,11 +21,6 @@ func update_bar(hp, max_hp : int):
 		return
 	
 	_hp_bar.value = float(hp)
-	var is_critical = _hp_bar.value < (_hp_bar.max_value * 0.25)
-	if is_critical and _hp_bar.value > 1:
-		_animation.play("beep")
-	else:
-		_animation.play("RESET")
 	
 func set_hp_bar_color(_color : Color):
 	_hp_bar.tint_progress = _color
