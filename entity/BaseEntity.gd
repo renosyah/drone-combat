36de8a3f536ef28e55287e4329e8814008a900a3
renosyah <syahputrareno975@gmail.com>
2022-09-21 09:54:25 +0700

    change to using only process

--- entity/BaseEntity.gd
+++ entity/BaseEntity.gd
@@ -74,7 +74,7 @@ func _ready():
 	
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _physics_process(delta):
+func _process(delta):
 	moving(delta)
 	
 	if not _is_network_running():
